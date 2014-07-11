82604f5d8af2b25b7cb1bc238f8e850a46bd694e
DLitvin <stayeronglass@gmail.com>
2014-07-11 18:37:01 +0400

    222

--- algoritm.php
+++ algoritm.php
@@ -1,2 +1,24 @@
 <?php
 
+$i = 0;
+
+function nextItem(){
+    global $i;
+
+    $result = sin($i);
+    $i += 0.1;
+    return $result;
+}
+
+function fillInput(){
+    $vector = array();
+    for($i = 0; $i < 10; $i++){
+        $vector[] = nextItem();
+    }
+
+    return $vector;
+}
+
+$input = fillInput();
+
+var_dump($input);
