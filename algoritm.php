7e061fcceeccd8f011f53e28768a1c8cbd3fc5aa
DLitvin <stayeronglass@gmail.com>
2014-07-22 19:17:32 +0400

    444

--- algoritm.php
+++ algoritm.php
@@ -2,15 +2,26 @@
 
     $i = 0;
     $n = 10;
+    $chartdataX = array();
+    $chartdataY = array();
+
+    $pointdataX = array();
+    $pointdataY = array();
+
     function nextItem(){
         global $i;
         global $n;
+        global $chartdataX;
+        global $chartdataY;
 
         $result = sin($i);
+        $chartdataX[] = $result;
+        $chartdataY[] =  $i;
+
         $i += 0.1;
 
-        if($i > $n ) return false;
-var_dump($result);
+        if($i > 13 ) return false;
+//var_dump($result);
         return $result;
     }//function nextItem(){
 
@@ -63,6 +74,9 @@ var_dump($result);
 
 
     $input = fillInput();
+
+    $chart = clone $input;
+
     $lastSituation = 'Buy';
     $sum = 0;
     $lastBuy = 0;
@@ -72,23 +86,80 @@ var_dump($result);
 
 
         if( situationChangedToBuy($input) && ('Sell' == $lastSituation) ):
-            var_dump('>>>>>>>>>>>>>situationChangedToBuy');
+            //var_dump('>>>>>>>>>>>>>situationChangedToBuy');
+            $pointdataX[] = $next;
+            $pointdataY[] = $i;
+
             $lastSituation = 'Buy';
             $lastBuy = $next;
             $sum = $sum + ($lastSell - $next);
         elseif( situationChangedToSell($input) && ('Buy' == $lastSituation) ):
-            var_dump('>>>>>>>>>>>>>situationChangedToSell');
+            //var_dump('>>>>>>>>>>>>>situationChangedToSell');
+            $pointdataX[] = $next;
+            $pointdataY[] = $i;
+
             $lastSituation = 'Sell';
             $lastSell = $next;
             $sum = $sum + ($next - $lastBuy);
         else:
+            $pointdataX[] = 0;
+            $pointdataY[] = $i;
 
         endif;
 
         $input->dequeue();
 
         $input->enqueue($next);
+        $chart->enqueue($next);
 
     endwhile;
+    require_once ('jpgraph/jpgraph.php');
+    require_once ('jpgraph/jpgraph_line.php');
+    require_once ('jpgraph/jpgraph_bar.php');
+    require_once ('jpgraph/jpgraph_iconplot.php');
+    require_once ('jpgraph/jpgraph_plotline.php');
+    require_once ('jpgraph/jpgraph_scatter.php');
+    require_once ('jpgraph/jpgraph_regstat.php');
 
-    var_dump($sum);
+
+// Setup the graph
+    $graph = new Graph(2500,500);
+    $graph->SetMargin(30,20,60,20);
+    $graph->SetMarginColor('white');
+    $graph->SetScale("linlin");
+
+// Hide the frame around the graph
+    $graph->SetFrame(false);
+
+// Setup title
+    $graph->title->Set("SIN(X)");
+    $graph->title->SetFont(FF_VERDANA,FS_BOLD,14);
+
+
+
+// Format the legend box
+    $graph->legend->SetColor('navy');
+    $graph->legend->SetFillColor('lightgreen');
+    $graph->legend->SetLineWeight(1);
+    $graph->legend->SetFont(FF_ARIAL,FS_BOLD,8);
+    $graph->legend->SetShadow('gray@0.4',3);
+    $graph->legend->SetAbsPos(15,120,'right','bottom');
+
+    $p2 = new LinePlot($pointdataX, $pointdataY);
+
+
+    $p2->mark->SetType(MARK_IMG_MBALL,'red');
+    $graph->Add($p2);
+
+    $p2->SetWeight(0);
+
+    $mainline = new LinePlot($chartdataX, $chartdataY);
+    $graph->Add($mainline);
+    $mainline->value->Show();
+
+
+
+    $graph->Stroke();
+
+
+
